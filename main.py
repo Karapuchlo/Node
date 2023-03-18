1b385b6346328d854a5deb0b11905919bb0c96c9
Aleksey <mc_gra_dy@mail.ru>
2023-03-18 23:21:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Для начала создать класс узла в виде:
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None 
+
+#Затем создать класс Stack, который будет содержать атрибут top, хранящий ссылку на верхний узел стека:
+
+class Stack:
+    def __init__(self):
+        self.top = None 
+
+    def push(self, data):
+        node = Node(data)
+        node.next = self.top
+        self.top = node
+#Здесь мы создаем экземпляр класса Node с аргументом data и добавляем его к стеку путем создания связи между этим уз
