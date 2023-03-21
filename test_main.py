bf0a1394c0987e8f7f28d5e1e078bf11035c4787
Aleksey <mc_gra_dy@mail.ru>
2023-03-21 15:42:42 +0300

    добавил корректные тесты

--- test_main.py
+++ test_main.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
-
+from main import Node, Stack
 
 class TestNode(TestCase):
+    n1 = Node(5)
+    assert n1 == 5
     pass
 
 
 class TestStack(TestCase):
+    stack = Stack()
     def test_push(self):
-        self.fail()
+        stack.test_push('data2') == 'data3'
 
